57c8fe46154e5e3ca1756ca296341efe373e5c8e
Timothy Beck <johndaveleet@gmail.com>
2021-02-27 17:21:02 -0700

    made parser dynamically get year for file names from data

--- parseLogs/parseLogs.js
+++ parseLogs/parseLogs.js
@@ -12,16 +12,19 @@ let arr = JSON.parse(one).records.concat(
 	JSON.parse(six).records)
 //import json above
 
+//gets the year for file names from the first entry
+let year = arr[0].dateOfStamp.substr(0,4);
+
 //sort json by date asc
 arr = arr.sort((a,b)=> {return a.dateOfActivity.replace(/-/g,'') - b.dateOfActivity.replace(/-/g,'');});
 
 //records.JSON of all data
 let json = JSON.stringify(arr);
-require("fs").writeFileSync('../logs/2007/2007records.json', json);
+require("fs").writeFileSync(`../logs/${year}/${year}records.json`, json);
 
 //SET of unique active dates
 let listUniqueDates = new Set();
-//records.md of all data
+//readme.md with all data
 let md = "| **Date** | **Image** |  **Description** | **wbmURL** | **Backup** |\n|:---------:|:---:|:----------:|:---:|:---:|\n";
 for(let i=0;i<arr.length;i++) {
  	md +=	`| ${arr[i].dateOfActivity} ` +
@@ -29,7 +32,7 @@ for(let i=0;i<arr.length;i++) {
 	`| [:link:](${arr[i].wbmURL}) | [:floppy_disk:](${arr[i].backupURL}) |\n`
 	listUniqueDates.add(arr[i].dateOfActivity);
 }
-require("fs").writeFileSync('../logs/2007/README.md', md);
+require("fs").writeFileSync(`../logs/${year}/README.md`, md);
 
 
 //records.CSV of all data
@@ -42,10 +45,10 @@ try {
 	const parser = new Parser(opts);
 	const csv = parser.parse(arr);
 	//console.log(csv);
-	require("fs").writeFileSync('../logs/2007/2007records.csv', csv);
+	require("fs").writeFileSync(`../logs/${year}/${year}records.csv`, csv);
 } catch (err) {
 	console.error(err);
 }
 
 //SET of unique active dates
-//console.log(listUniqueDates);
+console.log(listUniqueDates);
